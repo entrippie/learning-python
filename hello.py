036afc94844f9118a7f3788a1285a997461393f1
Entrippie <camacho.de7@gmail.com>
2025-03-13 16:48:14 -0400

    Learning Python: hello, world project and functions & variables

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-print("hello, world.")
+# print("hello, world.")
+#
+# name = input("What is your name? ")
+# name = name.strip().title()
+# print(f"Hello, {name}!")
 
-name = input("What is your name? ")
-name = name.strip().title()
-print(f"Hello, {name}!")
 
+# first, last = name.split()
+# print(f"Hello, {first}!")
 
-first, last = name.split()
-print(f"Hello, {first}!")
+def hello(to):
+    print("hello,", to)
+
+name = input("What's your name? ").title()
+hello(name)
